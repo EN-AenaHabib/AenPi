be6c303befcb826b2dc16b80458aff59381c7197
AenaEN- <aenahabibf23@nutech.edu.pk>
2026-05-09 15:01:36 +0500

    Update __init__.py

--- AenPi/urdu/__init__.py
+++ AenPi/urdu/__init__.py
@@ -1,10 +1,15 @@
-"""
-AenPi - Lightweight Urdu NLP Library
-Usage: from AenPi import urdu
-"""
+from AenPi.urdu.preprocessor import preprocess, tokenize, remove_punctuation
+from AenPi.urdu.stopwords import remove_stopwords, URDU_STOPWORDS, get_stopwords
+from AenPi.urdu.normalizer import normalize
+from AenPi.urdu.spell_corrector import spell_correct, spell_correct_text, edit_distance
+from AenPi.urdu.ngram import NGramPredictor, ngram_predict
+from AenPi.urdu.green_metrics import green_metrics, GreenMetrics
 
-from AenPi import urdu
-
-__version__ = "0.1.0"
-__author__ = "AenPi"
-__all__ = ["urdu"]
+__all__ = [
+    "preprocess", "tokenize", "remove_punctuation",
+    "remove_stopwords", "URDU_STOPWORDS", "get_stopwords",
+    "normalize",
+    "spell_correct", "spell_correct_text", "edit_distance",
+    "NGramPredictor", "ngram_predict",
+    "green_metrics", "GreenMetrics",
+]
