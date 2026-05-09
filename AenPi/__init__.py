190c450b63a75e4fd58aba16e21b89a84460f966
AenaEN- <aenahabibf23@nutech.edu.pk>
2026-05-09 14:56:41 +0500

    Simplify AenPi imports and update __all__
    Refactor AenPi package to simplify imports and expose only the urdu subpackage.

--- AenPi/__init__.py
+++ AenPi/__init__.py
@@ -1,26 +1,4 @@
-"""
-AenPi.urdu - Urdu NLP Subpackage
-"""
+from AenPi import urdu
 
-from .urdu.preprocessor import preprocess, tokenize, remove_punctuation
-from .urdu.stopwords import remove_stopwords, URDU_STOPWORDS
-from .urdu.normalizer import normalize
-from .urdu.spell_corrector import spell_correct, spell_correct_text, edit_distance
-from .urdu.ngram import NGramPredictor, ngram_predict
-from .urdu.green_metrics import green_metrics, GreenMetrics
-
-__all__ = [
-    "preprocess",
-    "tokenize",
-    "remove_punctuation",
-    "remove_stopwords",
-    "URDU_STOPWORDS",
-    "normalize",
-    "spell_correct",
-    "spell_correct_text",
-    "edit_distance",
-    "NGramPredictor",
-    "ngram_predict",
-    "green_metrics",
-    "GreenMetrics",
-]
+__version__ = "0.1.1"
+__all__ = ["urdu"]
