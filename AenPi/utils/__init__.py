0ebc3ee10dd551d19eae9163ff60fba9e0f1a8c0
AenaEN- <aenahabibf23@nutech.edu.pk>
2026-06-01 09:39:50 +0500

    Initialize utils module with green_metrics import

---
+++ AenPi/utils/__init__.py
@@ -0,0 +1,15 @@
+"""
+AenPi.utils
+-----------
+Utility tools for benchmarking, profiling, and developer diagnostics.
+"""
+
+from .green_metrics import (
+    GreenMetrics,
+    green_metrics,
+)
+
+__all__ = [
+    "GreenMetrics",
+    "green_metrics",
+]
