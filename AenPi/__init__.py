8d2fed2d1a1adbd245418b7fa3313647ce6109e8
AenaEN- <aenahabibf23@nutech.edu.pk>
2026-06-02 20:10:03 +0500

    Update __init__.py

--- AenPi/__init__.py
+++ AenPi/__init__.py
@@ -1,5 +1,5 @@
 from AenPi import urdu
 from .urdu import UrduNER  
-from .experimental import CarbonEstimator  
+
 __version__ = "0.1.1"
-__all__ = ["urdu", "UrduNER", "CarbonEstimator"]
+__all__ = ["urdu", "UrduNER"]
