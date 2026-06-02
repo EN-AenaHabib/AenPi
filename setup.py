701405345cc66b12a61cc9f169d37b07404c5161
AenaEN- <aenahabibf23@nutech.edu.pk>
2026-06-02 09:42:11 +0500

    Bump version to 0.1.2 and update package data
    Updated version number and package data to include joblib files.

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
-
 setup(
     name="AenPi",
-    version="0.1.1",
+    version="0.1.2",
     description="Lightweight Urdu NLP Library",
     packages=[
         "AenPi",
         "AenPi.urdu",
     ],
     package_data={
-        "AenPi.urdu": ["*.py", "aenpi_pos_crf.pkl.gz"],  # merged into one key
+        "AenPi.urdu": ["*.py", "*.pkl.gz", "*.joblib"],  # ← added *.joblib
     },
     include_package_data=True,
     install_requires=[
         "psutil>=5.9.0",
-        "sklearn-crfsuite",  # ← added
+        "sklearn-crfsuite",
     ],
     python_requires=">=3.7",
 )
